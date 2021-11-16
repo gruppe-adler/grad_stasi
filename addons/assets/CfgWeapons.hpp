b2be4505f6aca73112deec921289c142d2e2d968
nomisum <nomisum@gmail.com>
2021-11-16 18:35:02 +0100

    fix path

--- addons/assets/CfgWeapons.hpp
+++ addons/assets/CfgWeapons.hpp
@@ -22,9 +22,9 @@ class cfgWeapons {
         displaynameShort = "Handheld Mic";
         descriptionShort = "Handheld Mic";
 
-        model = "grad_stasi\addons\assets\data\models\mic.p3d";
-        picture="grad_stasi\addons\assets\data\textures\mic.paa";
-        UiPicture="grad_stasi\addons\assets\data\textures\mic.paa";
+        model = "\grad_stasi\assets\data\models\mic.p3d";
+        picture="\grad_stasi\assets\data\textures\mic.paa";
+        UiPicture="\grad_stasi\assets\data\textures\mic.paa";
 
         optics=0;
         weaponInfoType = "GRAD_stasi_rscOptics_mic";
